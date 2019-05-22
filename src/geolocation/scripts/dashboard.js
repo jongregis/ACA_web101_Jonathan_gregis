1f445a99a2d9ac579ba8ba5b81b45aefd51bbc43
jonathan gregis <jongregis@jonathans-MacBook-Pro.local>
2019-05-22 14:22:38 -0500

    last save

---
+++ src/geolocation/scripts/dashboard.js
@@ -0,0 +1,96 @@
+// Create a "close" button and append it to each list item
+var myNodelist = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < myNodelist.length; i++) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  myNodelist[i].appendChild(span);
+}
+
+// closes button to hide the current list item
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  };
+}
+
+// Add a "checked" symbol when clicking on a list item
+// var list = document.querySelector("ul");
+// list.addEventListener(
+//   "click",
+//   function(ev) {
+//     if (ev.target.tagName === "LI") {
+//       ev.target.classList.toggle("checked");
+//     }
+//   },
+//   false
+// );
+
+// Create a new list item when clicking on the "Add" button
+function newElement() {
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("myInput").value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === "") {
+    alert("You need to write something before you can add it to the list!");
+  } else {
+    document.getElementById("myUL").appendChild(li);
+  }
+  document.getElementById("myInput").value = "";
+
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      var div = this.parentElement;
+      div.style.display = "none";
+    };
+  }
+}
+
+// ---------------------------------geolocation
+
+var myLocation = document.getElementById("myloc");
+console.log(myLocation);
+function getLocation() {
+  navigator.geolocation.getCurrentPosition(success, error, options);
+}
+function displayPosition(position) {
+  var myLocation = document.getElementById("myloc");
+  myLocation.innerHTML =
+    "<strong>Longitude: </strong>" +
+    position.longitude +
+    "<br> <strong>Latitude: </strong>" +
+    position.latitude;
+}
+
+var options = {
+  enableHighAccuracy: true,
+  timeout: 5000,
+  maximumAge: 0
+};
+
+function success(pos) {
+  var crd = pos.coords;
+  displayPosition(crd);
+
+  console.log("Your current position is:");
+  console.log(`Latitude : ${crd.latitude}`);
+  console.log(`Longitude: ${crd.longitude}`);
+  console.log(`More or less ${crd.accuracy} meters.`);
+}
+
+function error(err) {
+  myLocation.innerText = "Location Tracking Not Possible.";
+  console.warn(`ERROR(${err.code}): ${err.message}`);
+}
